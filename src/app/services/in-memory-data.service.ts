48728437b8f4a91da7fddfc4af20b4c680947c6c
Mathieu le bellec <mathieu.le.bellec@gmail.com>
2022-11-24 22:45:39 +0100

    feat: annif toinou

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -4,16 +4,17 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 import * as questionsPapa from './questions/questionsPapa.json';
 import * as questionsFlo from './questions/questionsFlo.json';
 import * as questionsThibu from './questions/questionsThibu.json';
+import * as questionsToinou from './questions/questionsToinou.json';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb(): any {
-    const questions = (questionsThibu as any as FileBdd).questions;
-    const title = (questionsThibu as any as FileBdd).title;
-    const age = (questionsThibu as any as FileBdd).birthday;
-    const txtfin = (questionsThibu as any as FileBdd).txtFin;
+    const questions =  (questionsToinou as any as FileBdd).questions;
+    const title = (questionsToinou as any as FileBdd).title;
+    const age = (questionsToinou as any as FileBdd).birthday;
+    const txtfin = (questionsToinou as any as FileBdd).txtFin;
     return { questions, title, age, txtfin };
   }
 }
