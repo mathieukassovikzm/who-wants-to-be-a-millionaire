73ffd133360f77e840e8909f8bc1edcb2ff95439
Mathieu le bellec <mathieu.le.bellec@gmail.com>
2023-03-23 19:36:14 +0100

    feat: derniers reglages montée de version angular15

--- src/app/services/in-memory-data.service.ts
+++ src/app/services/in-memory-data.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
 import { FileBdd } from '@app/models/file-bdd';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
-import * as questionsPapa from './questions/questionsPapa.json';
-import * as questionsFlo from './questions/questionsFlo.json';
-import * as questionsThibu from './questions/questionsThibu.json';
-import * as questionsToinou from './questions/questionsToinou.json';
+// import jsonQuestions from './questions/questionsPapa.json';
+// import jsonQuestions from './questions/questionsFlo.json';
+// import jsonQuestions from './questions/questionsThibu.json';
+// import jsonQuestions from './questions/questionsToinou.json';
+import jsonQuestions from './questions/questionsExemple.json';
 
 @Injectable({
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
   createDb(): any {
-    const questions =  (questionsToinou as any as FileBdd).questions;
-    const title = (questionsToinou as any as FileBdd).title;
-    const age = (questionsToinou as any as FileBdd).birthday;
-    const txtfin = (questionsToinou as any as FileBdd).txtFin;
+    const questions = (jsonQuestions as any as FileBdd).questions;
+    const title = (jsonQuestions as any as FileBdd).title;
+    const age = (jsonQuestions as any as FileBdd).birthday;
+    const txtfin = (jsonQuestions as any as FileBdd).txtFin;
     return { questions, title, age, txtfin };
   }
 }
