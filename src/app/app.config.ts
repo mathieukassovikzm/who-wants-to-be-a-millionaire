063cc2c0c84a63e1e34771b9f9ad4d30eaf5571e
Mathieu Le Bellec <macpromath@mac.home>
2026-02-17 21:38:46 +0100

    feat: update angular v20 + application démarre

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app-routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
+    provideHttpClient(),
   ]
 };
